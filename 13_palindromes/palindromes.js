3eb8be270d8a8e32f31f9b3823cba9b5a0290465
Your Name <yourname@example.com>
2025-05-17 23:02:03 +0530

    Commit

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //remove punctuation,spaces,and convert to Lowercase
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g,'');
+
+
+//reverse string
+const reversed = cleaned.split('').reverse().join('');
+
+return cleaned === reversed;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0) return 'Error';
+
+    if(n===1||n===2){
+        return 1;
+    }
+
+    let a=1,b=1,temp;
+
+    for(let i=3;i<=n;i++){
+        temp = a+b;
+        a=b;
+        b=temp;
+    }
+    return b;
 
 };
 
